890968e569087b5196117e5d71271e4b214bb71e
filipplavac <fplavac1@gmail.com>
2021-01-12 11:22:13 +0100

    Login route disabled for users who are alerady logged in

--- routes/router.js
+++ routes/router.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import passport from 'passport';
-import {checkAuthenticated, checkFieldsFilled, checkRegistration, saveUserToDatabase} from './routerMiddleware.js';
+import {checkAuthenticated, checkLoggedIn, checkFieldsFilled, checkRegistration, saveUserToDatabase} from './routerMiddleware.js';
 
 const router  = express.Router();
 
 router.get('/', checkAuthenticated, (req, res) =>{
-    res.render('homepage', {user: req.user});
+    res.render('homepage', {title: 'AlbumsApp', user: req.user});
 })
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get('/login', checkLoggedIn, (req, res) => {
+    res.render('login', {title:'Login'});
 })
 
 router.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register', {title:'Register'});
 })
 
-router.get('/logout', (req,res) => {
+router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/login');
 });
@@ -31,6 +31,5 @@ router.post('/register', checkRegistration, saveUserToDatabase, (req, res) => {
     req.flash('success','Account successfully created, you can now Log In.');
     res.redirect('/login');
 });
-
 
 export default router;
--- routes/routerMiddleware.js
+++ routes/routerMiddleware.js
@@ -7,95 +7,103 @@ const checkAuthenticated = (req, res, next) => {
     } else {
         res.redirect('/login');
     }
-}
+};
+
+const checkLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        res.redirect('/');
+    } else {
+        next();
+    }
+};
 
 const checkFieldsFilled = (req, res, next) => {
-
+    
     let errors = [];
-
+    
     const {username,password} = req.body;
-
+    
     if(!username || !password){
         errors.push('Please enter all fields');
         res.render('login', {message: req.flash('error', errors)});
     } else {
         next();
     }
-
+    
 };
 
 const checkRegistration = (req, res, next) => {
-
+    
     let errors = [];
-
+    
     const {username,password} = req.body;
-
+    
     if(!username || !password){
         errors.push('Please enter all fields.')
     }
-
+    
     if(username && password && password.length < 6){
         errors.push('Password must have at least 6 characters');
     }
-
+    
     if(errors.length > 0){
         res.render('register', {message: req.flash('error', errors)});
         console.log(req.session.flash);
     } else {
         // Provjeri postoji li odabrano korisničko ime u bazi podataka
         User.findOne({username: username})
-            .then(user => {
-                if(user){
-                    errors.push('User with that username already exists, please try again.');
-                }
-
-                // Provjeri postoji li odabrana lozinka u bazi podataka
-                User.find()
-                    .then(users => {
-                        users.forEach(user => {
-                            if(checkPassword(password, user.hash, user.salt)){
-                                errors.push('Chosen password is already in use, please try again.')
-                            };
-                        });
-
-                        if(errors.length > 0){
-                            res.render('register', {message: req.flash('error', errors)});
-                        } else {
-                            next();
-                        };
-                    })
-                    .catch(err => {
-                        console.log(`\nAn error has occured while checking if password is available.\n\n${err}`);
-                        res.render('register');
-                    });
+        .then(user => {
+            if(user){
+                errors.push('User with that username already exists, please try again.');
+            }
+            
+            // Provjeri postoji li odabrana lozinka u bazi podataka
+            User.find()
+            .then(users => {
+                users.forEach(user => {
+                    if(checkPassword(password, user.hash, user.salt)){
+                        errors.push('Chosen password is already in use, please try again.')
+                    };
+                });
+                
+                if(errors.length > 0){
+                    res.render('register', {message: req.flash('error', errors)});
+                } else {
+                    next();
+                };
             })
             .catch(err => {
-                console.log(`\nAn error has occured while checking the username\n\n${err}`);
+                console.log(`\nAn error has occured while checking if password is available.\n\n${err}`);
                 res.render('register');
             });
-
+        })
+        .catch(err => {
+            console.log(`\nAn error has occured while checking the username\n\n${err}`);
+            res.render('register');
+        });
+        
     }
 };
 
 const saveUserToDatabase = (req, res, next) =>{
-     const saltAndHash = generatePassword(req.body.password),
-                        salt = saltAndHash.salt,
-                        hash = saltAndHash.hash;
-
-                    const newUser = new User({
-                        username: req.body.username,
-                        hash: hash,
-                        salt: salt
-                    });
-
-                    newUser.save()
-                        .then(user => {
-                            console.log(`\nUser successfully stored to the database\n\n${user}`);
-                            next();
-                        })
-                        .catch(err => {
-                            console.log(`\nAn error has occured while storing user to the database\n\n${err}`);
-                        });
+    const saltAndHash = generatePassword(req.body.password),
+          salt = saltAndHash.salt,
+          hash = saltAndHash.hash;
+    
+    const newUser = new User({
+        username: req.body.username,
+        hash: hash,
+        salt: salt
+    });
+    
+    newUser.save()
+    .then(user => {
+        console.log(`\nUser successfully stored to the database\n\n${user}`);
+        next();
+    })
+    .catch(err => {
+        console.log(`\nAn error has occured while storing user to the database\n\n${err}`);
+    });
 };
 
-export {checkAuthenticated, checkRegistration, checkFieldsFilled, saveUserToDatabase };
+export {checkAuthenticated, checkRegistration, checkFieldsFilled, saveUserToDatabase,checkLoggedIn};
